f21e411e41d57f3ea93eedf215b6f62f7f790c3e
Rafael Souza <rafael.souza@iexfy.com>
2024-05-15 11:29:58 -0300

    feat: create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 
+import { ModalProvider } from '@/components/providers/modal-provider'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             enableSystem
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
